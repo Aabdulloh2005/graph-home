1bae2a017ab37d3b8e2c6b11ef733dfa9584da20
Aabdulloh2005 <ganievabdulloh2005@gmail.com>
2024-07-23 20:04:14 +0500

    Graph

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+https://github.com/user-attachments/assets/73d4e0ed-0fb8-42e2-ae48-c7a8cc5e84ae
+
---
+++ lib/data/models/product.dart
@@ -0,0 +1,42 @@
+
+import 'package:lesson81_dio/data/models/category.dart';
+
+class Product {
+  final String id;
+  String title;
+  num price;
+  String description;
+  Category category;
+  List<String> images;
+
+  Product({
+    required this.id,
+    required this.title,
+    required this.price,
+    required this.description,
+    required this.category,
+    required this.images,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) {
+    return Product(
+      id: json['id'],
+      title: json['title'],
+      price: json['price'],
+      description: json['description'],
+      category: Category.fromJson(json),
+      images: List<String>.from(json['images']),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'price': price,
+      'description': description,
+      'category': category,
+      'images': images,
+    };
+  }
+}
